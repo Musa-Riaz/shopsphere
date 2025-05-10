a9d1b51abd507240487b7df36cbceefd853ee201
Musariaz <riazmusa69@gmail.com>
2025-05-11 01:20:00 +0500

    customized components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from "@/components/ui/textarea";
 export default function Home() {
   return (
-    <>
-    <p className="text-black text-xl shadow-2xs ">Hello World</p>
-    </>
+    <div className="p-4 ">
+    <Input placeholder="enter somethni" className="m-4" />
+    <Button  className="m-4" variant={"elevated"}>Hello</Button>
+    <Progress  className="m-4" value={50} />
+    <Textarea  className="m-4"> Hello</Textarea>
+    </div>
   );
 }
