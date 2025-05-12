3a35d8ea2b33c9680fd09790840fab6779152529
Musariaz <riazmusa69@gmail.com>
2025-05-12 15:05:57 +0500

    Added search filters

---
+++ src/app/(app)/(home)/search-filters/subcategory-menu.tsx
@@ -0,0 +1,47 @@
+import { Category } from "@/payload-types";
+import Link from "next/link";
+
+interface SubcategoryMenuProps {
+  category: Category;
+  isOpen: boolean;
+  position: { top: number; left: number };
+}
+
+export default function SubcategoryMenu({
+  category,
+  isOpen,
+  position,
+}: SubcategoryMenuProps) {
+  if (!isOpen || !category.subcategories || category.subcategories.length === 0)
+    return null;
+  const backgorundColor = category.color || "#f5f5f5";
+  return (
+    <div
+      className="fixed z-100"
+      style={{
+        top: position?.top,
+        left: position?.left,
+      }}
+    >
+      <div className="h-3 w-60" />
+      <div
+        className="w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_4px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]"
+        style={{
+          backgroundColor: backgorundColor || "#f5f5f5",
+        }}
+      >
+        <div>
+          {category.subcategories?.map((subcategory: Category) => (
+            <Link
+              key={subcategory.slug}
+              href="/"
+              className="w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center  font-medium"
+            >
+              {subcategory.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
